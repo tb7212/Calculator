ca253c2ea5dcdc47d998a5d626997318c9d51b28
Tasdiq Bashar <tasdiqbashar@gmail.com>
2025-06-21 11:20:22 -0400

    Complete calculator styling, colors, UI, and button placement

---
+++ calculator.js
@@ -0,0 +1,35 @@
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply (num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator){
+    switch (operator){
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return "Not a Smooth Operatorrrr!"
+    }
+}
